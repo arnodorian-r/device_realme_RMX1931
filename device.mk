60ade15ecf743253ea8e38fa3083ef84bd291e82
Danny Baumann <dannybaumann@web.de>
2019-01-22 14:15:40 +0100

    sm8150-common: Add touchscreen gestures HIDL HAL
    Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9

--- device.mk
+++ device.mk
@@ -491,6 +491,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.RMX1931
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
