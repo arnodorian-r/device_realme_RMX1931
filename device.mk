26152a148bc7236bc8e069f8dc52f2d2b723d233
arnodorian-r <geekroy98@gmail.com>
2020-11-28 20:33:06 +0530

    misc changes

--- device.mk
+++ device.mk
@@ -284,6 +284,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Misc
+PRODUCT_PACKAGES += \
+    libchrome \
+    libchrome.vendor
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1 \
