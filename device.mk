bbb94521b179f2ab570f4ea6895d4644b7caee6c
Pig <23kongw@brrice.edu>
2022-01-16 23:04:56 -0500

    raphael: Build android.hardware.tetheroffload.{config,control}@1.0 into vendor
    F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/ipacm": library "android.hardware.tetheroffload.config@1.0.so" not found: needed by main executable
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I019ad61ef8cbc0ce8267f5cb890e1f93e75fdddf

--- device.mk
+++ device.mk
@@ -526,6 +526,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Tetheroffload
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0.vendor \
+    android.hardware.tetheroffload.control@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
