0555d7b5cde85cc50dd516f239369034e37075ad
arnodorian-r <geekroy98@gmail.com>
2022-01-30 19:03:51 +0530

    nuke lineage fod implementation

--- device.mk
+++ device.mk
@@ -240,13 +240,11 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.RMX1931 \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.RMX1931 \
     vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
-#    vendor/dot/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:vendor/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # Framework detect
 PRODUCT_PACKAGES += \
