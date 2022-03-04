426dbec94fab3a88ce0eaf741a47fcdc88dbabbb
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    raphael: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba

--- device.mk
+++ device.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.common@6.0-util \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
