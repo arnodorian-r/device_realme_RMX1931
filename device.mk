ad12a784398ff7e7b8fa0c3f4492efc439ae5ffa
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:27:37 +0200

    mojito: Drop atrace HIDL
    Change-Id: Iaaaf62e9dda26dd15d51f5975d9f21fe1037d2eb

--- device.mk
+++ device.mk
@@ -90,10 +90,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# ATRACE_HAL
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
