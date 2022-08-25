8619f001ffbc8580acbe51b2d0863073fb6d3b9e
arnodorian-r <geekroy98@gmail.com>
2022-08-25 17:51:52 +0530

    cleanup rro_overlays

--- device.mk
+++ device.mk
@@ -548,11 +548,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    libqsap_sdk \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    WifiOverlay \
-    TetheringConfigOverlay \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
