e123ad129a05379c55bb6829d785af5b4b285fac
arnodorian-r <geekroy98@gmail.com>
2022-12-10 07:27:21 +0000

    temp: px changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+git clone https://github.com/arnodorian-r/kernel_realme_RMX1931.git kernel/realme/RMX1931
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/media
