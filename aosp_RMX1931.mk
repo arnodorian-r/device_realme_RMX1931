72aca90ec8251aea3d1b0afaf076b658c7b30985
arnodorian-r <geekroy98@gmail.com>
2023-02-24 15:00:58 +0530

    RMX1931: use common_full_phone build

--- aosp_RMX1931.mk
+++ aosp_RMX1931.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
