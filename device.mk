219713339ea43c4625f32e4b3a2f3b2e722e6994
arnodorian-r <ramanabandari@gmail.com>
2023-03-16 23:24:36 +0530

    RMX1931: add some camera related blobs to be found as public

--- device.mk
+++ device.mk
@@ -169,6 +169,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/realme/camera/camera-vendor.mk)
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt \
     $(LOCAL_PATH)/configs/permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
     $(LOCAL_PATH)/configs/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml
