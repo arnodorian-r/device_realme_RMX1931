abb481bba408c417c555604e7d0e0e8c92c74759
arnodorian-r <geekroy98@gmail.com>
2023-04-21 21:35:45 +0530

    RMX1931: Minimalize fwk and move to Oppo camera prop

--- oplus-fwk/src/android/content/res/OplusBaseConfiguration.java
+++ oplus-fwk/src/android/content/res/OplusBaseConfiguration.java
@@ -3,11 +3,7 @@ package android.content.res;
 import oplus.content.res.OplusExtraConfiguration;
 
 public abstract class OplusBaseConfiguration {
-    public OplusExtraConfiguration mOplusExtraConfiguration;
-
-    public OplusBaseConfiguration(OplusExtraConfiguration oplusExtraConfiguration) {
-        mOplusExtraConfiguration = oplusExtraConfiguration;
-    }
+    public OplusExtraConfiguration mOplusExtraConfiguration = new OplusExtraConfiguration();
 
     public OplusExtraConfiguration getOplusExtraConfiguration() {
         return this.mOplusExtraConfiguration;
--- oplus-fwk/src/com/oplus/os/OplusBuild.java
+++ oplus-fwk/src/com/oplus/os/OplusBuild.java
@@ -1,9 +1,11 @@
 package com.oplus.os;
 
 public class OplusBuild {
-    public static final int OplusOS_11_3 = 22;
+    public static class VERSION {
+        public static final String RELEASE = "V11";
+    }
 
     public static int getOplusOSVERSION() {
-        return 23;
+        return 19;
     }
 }
